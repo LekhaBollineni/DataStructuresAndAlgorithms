8e770fa0077277109d72a2611c5d3f8fafc55f06
LekhaBollineni <lekhabollineni2022@gmail.com>
2025-09-10 15:40:50 -0400

    insertion Sort

---
+++ SortingAlgorithms/3_InsertionSort/inserstionSort.py
@@ -0,0 +1,21 @@
+from typing import List
+
+def insertion_sort(arr:List[int])->None:
+    
+    n = len(arr)
+    
+    for i in range(1,n):
+        key = arr[i]
+        j = j-1
+        
+        while j>=0 and arr[j] > key:
+            arr[j+1]=arr[j]
+            j-=1
+        
+        arr[j+1]=key
+    
+    if __name__ == "__main__":
+        input_array = list(map(int,input("Enter numbers separated by spaces: ").split()))
+        insertion_sort(input_array)
+        print("Sorted array:", input_array)
+            
